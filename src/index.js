45d567e2660b3a8b2d1038611e7f6637bbeeaae0
sirsegv <sirsegv@sirsegv.moe>
2025-10-28 16:31:55 +1030

    use dark bootstrap theme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './ui/App/App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
