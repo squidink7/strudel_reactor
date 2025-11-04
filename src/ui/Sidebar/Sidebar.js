4038b5c0a707e0b9c889e958196adb77023fa4c1
sirsegv <sirsegv@sirsegv.moe>
2025-11-04 14:44:17 +1030

    Add graph to sidebar (w.i.p.)

--- src/ui/Sidebar/Sidebar.js
+++ src/ui/Sidebar/Sidebar.js
@@ -1,23 +1,27 @@
 import { useState } from "react";
+import { Graph } from "../Graph/Graph"
 
-export function Sidebar({arrangements, arrangementId, setArrangementId}) {
+export function Sidebar({arrangements, arrangementId, setArrangementId, startGraph}) {
 
 	return (
-		<div className="d-flex flex-column w-25 bg-light border-end p-3">
-			<button className="btn btn-primary btn-lg mb-4" onClick={() => setArrangementId(-1)}>
-				All Parts
-			</button>
-			
-			<h5 className="mb-3">Arrangements</h5>
-			<div className="overflow-auto" style={{ maxHeight: 'calc(100vh - 150px)' }}>
-				<ul className="list-group">
-					{arrangements.map(arrangement => (
-						<button key={arrangement.id} type="button" className={"list-group-item list-group-item-action " + (arrangement.id == arrangementId ? "active" : "")} onClick={() => setArrangementId(arrangement.id)}>
-							{arrangement.name}
-						</button>
-					))}
-				</ul>
+		<div className="d-flex flex-column w-25 bg-light border-end p-3 justify-content-between">
+			<div className="d-flex flex-column">
+				<button className="btn btn-primary btn-lg mb-4" onClick={() => setArrangementId(-1)}>
+					All Parts
+				</button>
+				
+				<h5 className="mb-3">Arrangements</h5>
+				<div className="overflow-auto" style={{ maxHeight: 'calc(100vh - 150px)' }}>
+					<ul className="list-group">
+						{arrangements.map(arrangement => (
+							<button key={arrangement.id} type="button" className={"list-group-item list-group-item-action " + (arrangement.id == arrangementId ? "active" : "")} onClick={() => setArrangementId(arrangement.id)}>
+								{arrangement.name}
+							</button>
+						))}
+					</ul>
+				</div>
 			</div>
+			{/* <Graph start={startGraph} /> */}
 		</div>
 	)
 }
